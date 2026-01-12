849f8d455ae5cec0a25c61f0c30a95881964e242
MaximoPorretti <maximoporretti2016@gmail.com>
2026-01-12 15:09:14 -0300

    commitaso

--- src/main/java/org/example/vis/domain/enums/StaffRole.java
+++ src/main/java/org/example/vis/domain/enums/StaffRole.java
@@ -2,5 +2,7 @@ package org.example.vis.domain.enums;
 
 public enum StaffRole {
     ADMIN,
-    RECEPCIONISTA
+    RECEPCIONISTA,
+    ENTRENADOR
+    //muchas veces en los gym que eh ido el mismo entrenador hace de recepcionista
 }
